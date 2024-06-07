e04de449a7fa21f5c97a15430296000fca7b58a9
David Clinton <clinton@192.168.0.111>
2024-06-07 09:31:11 +0300

    feat: add async storage

--- app/camera/camera-page.tsx
+++ app/camera/camera-page.tsx
@@ -1,12 +1,14 @@
+import { Button, FocusAwareStatusBar } from "@/ui";
 import { CameraView, useCameraPermissions } from "expo-camera";
+import * as FileSystem from "expo-file-system";
 import { Stack } from "expo-router";
-import { useState } from "react";
-import { Button, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import React, { useRef } from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export default function App() {
-  const [facing, setFacing] = useState<any>("back");
   const [permission, requestPermission] = useCameraPermissions();
-
+  const cameraRef = useRef<CameraView | null>(null);
   if (!permission) {
     return (
       <View>
@@ -14,34 +16,55 @@ export default function App() {
       </View>
     );
   }
-
   if (!permission.granted) {
     return (
-      <View style={styles.container}>
+      <>
+        <FocusAwareStatusBar />
         <Stack.Screen options={{ headerShown: false }} />
-        <Text style={{ textAlign: "center" }}>
-          We need your permission to show the camera
-        </Text>
-        <Button onPress={requestPermission} title="grant permission" />
-      </View>
+        <View className="flex-1 justify-center items-center p-6">
+          <Text className="text-md ml-6 mt-4 font-semibold">
+            We need your permission to show the camera
+          </Text>
+          <Button
+            variant="primary"
+            onPress={requestPermission}
+            label="Grant Permission"
+          />
+        </View>
+      </>
     );
   }
 
-  function toggleCameraFacing() {
-    setFacing((current: string) => (current === "back" ? "front" : "back"));
-  }
+  const takePicture = async () => {
+    if (cameraRef.current) {
+      const photo = await cameraRef.current.takePictureAsync();
+      const fileName = `${FileSystem.documentDirectory}photo_${Date.now()}.jpg`;
+
+      try {
+        await FileSystem.moveAsync({
+          from: photo?.uri as any,
+          to: fileName,
+        });
+
+        // Store the photo data in AsyncStorage
+        await AsyncStorage.setItem("capturedPhoto", fileName);
+        console.log("Photo saved to", fileName);
+      } catch (e) {
+        console.log("Error saving photo", e);
+      }
+    }
+  };
 
   return (
-    <View style={styles.container}>
+    <>
       <Stack.Screen options={{ headerShown: false }} />
-      <CameraView style={styles.camera} facing={facing}>
-        <View style={styles.buttonContainer}>
-          <TouchableOpacity style={styles.button} onPress={toggleCameraFacing}>
-            <Text style={styles.text}>Flip Camera</Text>
-          </TouchableOpacity>
-        </View>
-      </CameraView>
-    </View>
+      <View className="flex-1 justify-center items-center p-6">
+        <CameraView style={styles.camera} facing="back" ref={cameraRef} />
+        <TouchableOpacity onPress={takePicture} style={styles.captureButton}>
+          <Text style={styles.captureButtonText}>Capture</Text>
+        </TouchableOpacity>
+      </View>
+    </>
   );
 }
 
@@ -53,20 +76,14 @@ const styles = StyleSheet.create({
   camera: {
     flex: 1,
   },
-  buttonContainer: {
-    flex: 1,
-    flexDirection: "row",
-    backgroundColor: "transparent",
-    margin: 64,
+  captureButton: {
+    backgroundColor: "white",
+    padding: 10,
+    borderRadius: 25,
+    marginBottom: 20,
   },
-  button: {
-    flex: 1,
-    alignSelf: "flex-end",
-    alignItems: "center",
-  },
-  text: {
-    fontSize: 24,
+  captureButtonText: {
+    fontSize: 16,
     fontWeight: "bold",
-    color: "white",
   },
 });
