16b084b403efcd0aaea4364ecd6deb08ea1c10db
David Clinton <clinton@192.168.0.103>
2024-08-21 14:04:33 +0300

    feat: add moti transition

--- app/camera/camera-page.tsx
+++ app/camera/camera-page.tsx
@@ -9,6 +9,7 @@ import { showMessage } from "react-native-flash-message";
 import CameraRoll from "@/components/camera-roll";
 import { uploadImageAsync } from "@/api/ImagePicker";
 import { Feather, MaterialCommunityIcons } from "@expo/vector-icons";
+import { MotiView } from "moti";
 
 export default function CameraPage() {
   const [permission, requestPermission] = useCameraPermissions();
@@ -81,55 +82,70 @@ export default function CameraPage() {
   return (
     <>
       <View className="flex-1 items-center py-5">
-        <CameraView
-          style={fullCamera ? styles.fullCamera : styles.camera}
-          facing="back"
-          ref={cameraRef}
+        <MotiView
+          from={{
+            height: fullCamera ? "50%" : "90%",
+            width: "100%",
+            borderRadius: fullCamera ? 20 : 0,
+          }}
+          animate={{
+            height: fullCamera ? "90%" : "50%",
+            width: "100%",
+            borderRadius: fullCamera ? 0 : 20,
+          }}
+          transition={{ type: "timing", duration: 300 }}
         >
-          <View style={styles.buttonContainer}>
-            <View className="flex-row mt-10 justify-between">
-              <Pressable
-                onPress={() => {
-                  handleCancel();
-                }}
-              >
-                <Feather name="x" size={24} color="white" />
-              </Pressable>
-              <Text className="text-white font-bold">Snaplist Lens</Text>
-              <MaterialCommunityIcons
-                name="dots-vertical"
-                size={24}
-                color="white"
-              />
+          <CameraView
+            style={fullCamera ? styles.fullCamera : styles.camera}
+            facing="back"
+            ref={cameraRef}
+          >
+            <View style={styles.buttonContainer}>
+              <View className="flex-row mt-10 justify-between">
+                <Pressable
+                  onPress={() => {
+                    handleCancel();
+                  }}
+                >
+                  <Feather name="x" size={24} color="white" />
+                </Pressable>
+                <Text className="text-white font-bold">Snaplist Lens</Text>
+                <MaterialCommunityIcons
+                  name="dots-vertical"
+                  size={24}
+                  color="white"
+                />
+              </View>
+              {!fullCamera ? (
+                <View className={`flex-1 items-center justify-center`}>
+                  <Pressable
+                    onPress={() => {
+                      setFullCamera(!fullCamera);
+                    }}
+                    className="w-[60px] h-[60px] bg-transparent border-lime-100 border-2 justify-center items-center rounded-full"
+                  >
+                    <Feather name="camera" size={24} color="white" />
+                  </Pressable>
+                  <Text className="text-white mt-5 font-bold">
+                    Search with your camera
+                  </Text>
+                </View>
+              ) : (
+                <View className="flex-1 justify-end items-center mb-10">
+                  <Pressable
+                    onPress={() => {
+                      setFullCamera(!fullCamera);
+                    }}
+                    className="w-[60px] h-[60px] bg-transparent border-lime-100 border-2 justify-center items-center rounded-full"
+                  >
+                    <Feather name="camera" size={24} color="white" />
+                  </Pressable>
+                </View>
+              )}
             </View>
-            {!fullCamera ? (
-              <View className={`flex-1 items-center justify-center`}>
-                <Pressable
-                  onPress={() => {
-                    setFullCamera(!fullCamera);
-                  }}
-                  className="w-[60px] h-[60px] bg-transparent border-lime-100 border-2 justify-center items-center rounded-full"
-                >
-                  <Feather name="camera" size={24} color="white" />
-                </Pressable>
-                <Text className="text-white mt-5 font-bold">
-                  Search with your camera
-                </Text>
-              </View>
-            ) : (
-              <View className="flex-1 justify-end items-center mb-10">
-                <Pressable
-                  onPress={() => {
-                    setFullCamera(!fullCamera);
-                  }}
-                  className="w-[60px] h-[60px] bg-transparent border-lime-100 border-2 justify-center items-center rounded-full"
-                >
-                  <Feather name="camera" size={24} color="white" />
-                </Pressable>
-              </View>
-            )}
-          </View>
-        </CameraView>
+          </CameraView>
+        </MotiView>
+
         {!fullCamera && <CameraRoll />}
       </View>
     </>
@@ -142,9 +158,9 @@ const styles = StyleSheet.create({
     justifyContent: "center",
   },
   camera: {
-    flex: 0.5,
+    flex: 0.9,
     width: "100%",
-    borderRadius: 20,
+    borderRadius: 15,
   },
   fullCamera: {
     flex: 0.9,
